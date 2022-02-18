4e240f35d4a6a9d147dbba16423b7a8763e576da
Giscard Biamby <giscard@gmail.com>
2022-02-18 06:22:50 +0000

    asdf

--- scripts/get_video_detection.sh
+++ scripts/get_video_detection.sh
@@ -1,16 +1,20 @@
-clear && python get_video_detections.py --device=0 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_010_extra_augs--geoscreens_010-model_faster_rcnn-bb_resnest50_fpn-024f52f6dd
-clear && python get_video_detections.py --device=1 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_010_extra_augs--geoscreens_010-model_faster_rcnn-bb_resnest50_fpn-024f52f6dd
-clear && python get_video_detections.py --device=2 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_010_extra_augs--geoscreens_010-model_faster_rcnn-bb_resnest50_fpn-024f52f6dd
-clear && python get_video_detections.py --device=3 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_010_extra_augs--geoscreens_010-model_faster_rcnn-bb_resnest50_fpn-024f52f6dd
-clear && python get_video_detections.py --device=4 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_010_extra_augs--geoscreens_010-model_faster_rcnn-bb_resnest50_fpn-024f52f6dd
-clear && python get_video_detections.py --device=5 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_010_extra_augs--geoscreens_010-model_faster_rcnn-bb_resnest50_fpn-024f52f6dd
-clear && python get_video_detections.py --device=6 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_010_extra_augs--geoscreens_010-model_faster_rcnn-bb_resnest50_fpn-024f52f6dd
-clear && python get_video_detections.py --device=7 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_010_extra_augs--geoscreens_010-model_faster_rcnn-bb_resnest50_fpn-024f52f6dd
-clear && python get_video_detections.py --device=8 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_010_extra_augs--geoscreens_010-model_faster_rcnn-bb_resnest50_fpn-024f52f6dd
-clear && python get_video_detections.py --device=9 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_010_extra_augs--geoscreens_010-model_faster_rcnn-bb_resnest50_fpn-024f52f6dd
+clear && python get_video_detections.py --device=0 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa
+clear && python get_video_detections.py --device=1 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa
+clear && python get_video_detections.py --device=2 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa
+clear && python get_video_detections.py --device=3 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa
+clear && python get_video_detections.py --device=4 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa
 
-clear && python get_video_detections.py --device=0 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_010_extra_augs--geoscreens_010-model_faster_rcnn-bb_resnest50_fpn-024f52f6dd --num_devices 5
-clear && python get_video_detections.py --device=1 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_010_extra_augs--geoscreens_010-model_faster_rcnn-bb_resnest50_fpn-024f52f6dd --num_devices 5
-clear && python get_video_detections.py --device=2 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_010_extra_augs--geoscreens_010-model_faster_rcnn-bb_resnest50_fpn-024f52f6dd --num_devices 5
-clear && python get_video_detections.py --device=3 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_010_extra_augs--geoscreens_010-model_faster_rcnn-bb_resnest50_fpn-024f52f6dd --num_devices 5
-clear && python get_video_detections.py --device=4 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_010_extra_augs--geoscreens_010-model_faster_rcnn-bb_resnest50_fpn-024f52f6dd --num_devices 5
+clear && python get_video_detections.py --device=5 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa
+clear && python get_video_detections.py --device=6 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa
+clear && python get_video_detections.py --device=7 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa
+clear && python get_video_detections.py --device=8 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa
+clear && python get_video_detections.py --device=9 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa
+
+# clear && python get_video_detections.py --device=0 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa --num_devices 5
+# clear && python get_video_detections.py --device=1 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa --num_devices 5
+# clear && python get_video_detections.py --device=2 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa --num_devices 5
+# clear && python get_video_detections.py --device=3 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa --num_devices 5
+# clear && python get_video_detections.py --device=4 --checkpoint_path /home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa --num_devices 5
+
+/home/gbiamby/proj/geoscreens/tools/output/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa
+/shared/gbiamby/geo/models/gs_011_extra_augs--geoscreens_011-model_faster_rcnn-bb_resnest50_fpn-3f36fb97fa
